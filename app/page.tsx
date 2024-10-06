396bf20d2783c41a9b5dcd333d4af964d2d8feae
Yashodeep Nimbekar <117455443+Yashodeeps@users.noreply.github.com>
2024-10-06 08:49:59 +0530

    Refactor page component and add SplineBall component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 "use client";
-import Spline from '@splinetool/react-spline/next';
+import Spline from "@splinetool/react-spline/next";
 import { Button } from "@/components/ui/button";
 import React, { useEffect } from "react";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import SplineBall from "@/components/SplineBall";
 
 const Page = () => {
   const session = useSession();
@@ -21,14 +22,19 @@ const Page = () => {
     if (session.data?.user) {
       router.push("/dashboard");
     }
-  }, [session, router, handleSignIn]);
+  }, [session, router]);
 
   return (
     <div className="flex justify-center items-center h-screen">
-      {/* Left side with the button */}
-      <div className="flex w-1/2 h-full justify-center items-center bg-gray-100">
-        <button 
-          className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+      <div className="flex flex-col pl-12  w-1/2 h-full justify-center items-start">
+        <div>
+          <h1 className="text-8xl font-bold ">Welcome to SoulTalk</h1>
+          <p className="text-2xl py-4 ">Your Personalized Therapy Platform</p>
+        </div>
+        <button
+          onClick={handleSignIn}
+          className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
             Sign Up to get started
@@ -38,10 +44,7 @@ const Page = () => {
 
       {/* Right side with the Spline animation */}
       <div className="w-1/2 h-full">
-        <Spline
-          scene="https://prod.spline.design/Xi0dkChssomQ82tS/scene.splinecode"
-          className="w-full h-full"
-        />
+        <SplineBall />
       </div>
     </div>
   );
